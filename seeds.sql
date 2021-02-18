fe18b4a2d09e08f49b3891a9a836080c4b35fe2f
Austin Ruhf <austin.ruhf@gmail.com>
2021-02-18 13:13:36 -0500

    added sql seeds & schema

---
+++ seeds.sql
@@ -0,0 +1,41 @@
+USE employee_db;
+
+INSERT INTO department (name) VALUES
+    ('Founders'),
+    ('Sales'),
+    ('Marketing'),
+    ('IT');
+
+INSERT INTO role (title, salary, department_id) VALUES
+    ('Chief Executive Officer', 140000, 1),
+    ('Account Executive', 92000, 2),
+    ('Sales Representative', 64000, 2),
+    ('Marketing Director', 78000, 3),
+    ('Marketing Designer', 63000, 3),
+    ('Social Media Intern', 20000, 3),
+    ('Server Administrator', 92000, 4),
+    ('Software Engineer', 76000, 4),
+    ('Tech Support', 48000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES
+    ('Bobson', 'Dugnutt', 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+    ('Sleve', 'McDichael', 2, 1),
+    ('Onson', 'Sweemey', 3, 2),
+    ('Darryl', 'Archideld', 3, 2),
+    ('Anatolia', 'Smorin', 3, 1),
+    ('Reya', 'McSriff', 4, 5),
+    ('Glen', 'Mixon', 5, 5),
+    ('Raul', 'Chamgerlain', 5, 5),
+    ('Scott', 'Dourque', 5, 5),
+    ('Willie', 'Dustice', 6, 5),
+    ('Mike', 'Truk', 6, 5),
+    ('Jeromy', 'Gride', 7, 1),
+    ('Dean', 'Wesrey', 8, 12),
+    ('Karla', 'Dandleton', 8, 12),
+    ('Mike', 'Sernandez', 8, 12),
+    ('Kevin', 'Nogilny', 8, 12),
+    ('Todd', 'Bonzalez', 9, 12),
+    ('Tony', 'Smehirk', 9, 12),
+    ('Eleanore', 'Leonard', 9, 12);
