a4fe01ac585a8dbd701f05be7f89492f5a62e986
Austin Ruhf <austin.ruhf@gmail.com>
2021-02-18 21:23:35 -0500

    connected mysql & inquirer

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,7 +19,7 @@ INSERT INTO role (title, salary, department_id) VALUES
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ('Bobson', 'Dugnutt', 1, null);
+    ('Bobson', 'Dugnutt', 1, null),
     ('Sleve', 'McDichael', 2, 1),
     ('Onson', 'Sweemey', 3, 2),
     ('Darryl', 'Archideld', 3, 2),
